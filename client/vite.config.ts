a549f14522015a476738a2d6da76e290599243fb
goz86 <goztrade86@gmail.com>
2026-06-02 11:56:39 +0900

    Fix production blank screen

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,29 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import obfuscator from 'vite-plugin-javascript-obfuscator'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    obfuscator({
-      apply: 'build',
-      exclude: [/node_modules/],
-      options: {
-        compact: true,
-        controlFlowFlattening: false,
-        controlFlowFlatteningThreshold: 0,
-        numbersToExpressions: false,
-        simplify: true,
-        stringArray: true,
-        stringArrayShuffle: true,
-        stringArrayThreshold: 0.35,
-        splitStrings: false,
-        splitStringsChunkLength: 10,
-      },
-    }),
   ],
   build: {
     sourcemap: false,
